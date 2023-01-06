59a9745a744a913c7bc4e1622db66024d79d9bed
Andrew <aat-78@mail.ru>
2023-01-06 14:18:44 +0300

    чистка

--- views.py
+++ views.py
@@ -9,10 +9,7 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/perform_query', methods=['POST'])
 def perform_query():
     data = request.json
-    try:
-        validated_data = BatchRequestSchema().load(data)
-    except ValidationError as error:
-        return jsonify(error.messages), 400
+    validated_data = BatchRequestSchema().load(data)
     result = None
     for query in validated_data:
         result = build_query(
